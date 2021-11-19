8fe0a7789f43fe5aaead287eb19af0ee43fa25b4
Ajizu1985 <az3406.aa@gmail.com>
2021-11-19 16:51:54 +0500

    New

---
+++ 15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[]="008186AD900t-=p9";
+    int i, count=0;
+    unsigned long length = strlen(str);
+    printf("Length:%zu\n", length);
+
+    for(i=0; str[i]!='\0'; i++){
+    if((str[i]>='a' && str[i]<='z')) {
+            count++;
+    }}
+        printf("Amount: %d\n", count);
+    return 0;
+}
