faa53520edf05d243c992c5ab01c4e7c239764ca
Ajizu1985 <az3406.aa@gmail.com>
2021-11-21 23:36:32 +0500

    new

--- 19.c
+++ 19.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
 int main()
 {
-    char sym[]="g", sym1="h";
-    bool symbol=true;
-    int i;
-    symbol = strchr(sym, '.');
-    if (symbol==false) {
+    char strn1[]="g";
+    bool symbol, val;
+    symbol = strchr(strn1, '.');
+    val = atoi(strn1);
+        if (val==0){
+        printf("Not a digit (0)\n");
+        }
+        if (symbol==0) {
         printf("Whole number(1)\n");
         }
-    if (symbol==true){
+        if (symbol==1){
         printf("Real number(2)\n");
     }
-   for(i=0; i<1; i++){
-        printf("Symbol can be transformed into digits (ASCII table)=%d\n", sym[0]);
-   }
-    for(i=0; i<1; i++){
-        printf("Symbol cannot be transformed into digits (0)=%d\n", sym[0]);
-   }
+  
+    
     return 0;
 }
