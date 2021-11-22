57fbd2971173ddbe7c19e07625a3e5be4f3fb94e
Ajizu1985 <az3406.aa@gmail.com>
2021-11-22 23:44:08 +0500

    New

--- 11.c
+++ 11.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 int main()
 {
-    char car[100];
-    printf("Enter car: ");
-    fgets(car, sizeof(car), stdin);
-    printf("Car:");
-    puts (car);
+    char Company[20];
+    int i, j, n;
+    int length;
+
+    printf("Enter Company: ");
+    fgets(Company, sizeof(Company), stdin);
+    length = strlen(Company);
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    //printf("%zu", length);
+    for (i=0;i<=length;i++){
+    printf("%c", Company[i]);
+    for(j=0;j<n;j++){
+      printf("  ");
+    }
+    printf("\t");
+    }
     return 0;
 }
