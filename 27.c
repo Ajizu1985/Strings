518fa81a68774d4c508489a6b3eed26d3486c1e9
Ajizu1985 <az3406.aa@gmail.com>
2021-11-23 14:43:13 +0500

    New

---
+++ 27.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main () {
+char str1[100] = ""Paxtakor;
+char str2[100] = "chempion! onasini emsin";
+strcat(str1, str2);
+printf("%s\n", str1);
+return 0;
+}
